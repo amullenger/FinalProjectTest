74f88f110e6df2f36caefd7322096f53d2737e38
Andrew Mullenger <amullenger@wustl.edu>
2019-07-07 22:25:57 -0500

    adding initial test files. css file not currently used in any html docs

---
+++ app.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import flask
+
+from flask import Flask, render_template, request
+
+from tensorflow.keras.models import load_model
+
+def predict_outcome(loan_info_list):
+    # Load model ***
+    ml_model = load_model("8Columns_DeepModel8_Trained.h5")
+    pred_data = np.array(loan_info_list).reshape(1, 8)
+    prediction = ml_model.predict(pred_data)
+    return prediction
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    """Render Home Page"""
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST"])
+def generate_prediction():
+    if request.method == "POST":
+        res_dict = request.form.to_dict()
+        res_list = list(res_dict.values())
+        res_list_mapped = list(map(float, res_list))
+
+        pred = predict_outcome(res_list_mapped)
+        
+        return render_template("result.html", result = pred)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
